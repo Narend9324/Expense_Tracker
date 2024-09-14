e594b0bf4f51d6561833abba04115b02c2585ca8
Narend9324 <nc379783@gmail.com>
2024-09-14 12:58:07 +0530

    Feat: Added Currency options & UI Fixes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ function App() {
     setExpenses([expense, ...expenses]);
   };
 
+  const totalAmount = expenses.reduce(
+    (total, expense) => total + expense.amount,
+    0
+  );
+
   return (
     <div className="App">
       <Header />
       <AddExpense onAdd={addExpenseHandler} />
       <ExpenseList expenses={expenses} />
+      <h2>Total: ₹ {totalAmount.toFixed(2)}</h2>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const ExpenseItem = ({ expense }) => {
+  const formattedAmount = expense.amount.toLocaleString("en-IN", {
+    style: "currency",
+    currency: "INR",
+  });
+
   return (
     <li>
-      {expense.title} - ${expense.amount}
+      {expense.title} - {formattedAmount} on {expense.date} [{expense.category}]
     </li>
   );
 };
